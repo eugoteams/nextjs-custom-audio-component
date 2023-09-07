2f0fd183cebb257a5d96a8eca321db10b0a5cf48
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-09-08 01:01:39 +0530

    remove AdComponent

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
         onPlayerPrevTrack={onPlayerPrevTrackListener}
         onLog={onLogListener}
       />
-      <AdComponent ref={audioRef} />
+      {/* <AdComponent ref={audioRef} /> */}
     </React.Fragment>
   );
 }
